8fe4dba4b1865ec2b2ef3f8ef75b7452b9514e92
VarshithGudala <84899041+VarshithGudala@users.noreply.github.com>
2025-02-10 21:04:38 -0600

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -63,7 +63,7 @@ export class LoginComponent {
  msalInstance = new PublicClientApplication({
   auth: {
     clientId: '00e599f2-c3c0-4fc2-9bba-29489f9a189b',
-    authority: 'https://login.microsoftonline.com/65e0ca54-5ef8-4817-9149-3d007c53fe35/oauth2/v2.0/authorize',
+    authority: 'https://login.microsoftonline.com/tenantId/oauth2/v2.0/authorize',
     redirectUri: 'http://localhost:4200',
     //  postLogoutRedirectUri: '/'
   },
