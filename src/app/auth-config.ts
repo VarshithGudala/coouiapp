54762a5119994d1fe4e7a55c72aaf68876e441bd
VarshithGudala <84899041+VarshithGudala@users.noreply.github.com>
2025-02-10 21:02:31 -0600

    Update auth-config.ts

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -2,7 +2,7 @@ import { BrowserCacheLocation, LogLevel } from '@azure/msal-browser';
 export const msalConfig = {
   auth: {
     clientId: '00e599f2-c3c0-4fc2-9bba-29489f9a189b',
-      authority: 'https://login.microsoftonline.com/65e0ca54-5ef8-4817-9149-3d007c53fe35/oauth2/v2.0/authorize',
+      authority: 'https://login.microsoftonline.com/TenantId/oauth2/v2.0/authorize',
       redirectUri: 'http://localhost:4200',
   },
   cache: {
